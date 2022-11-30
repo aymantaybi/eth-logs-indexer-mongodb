cec337e52c3008929b019d4c98ac743266720642
aymantaybi <42820584+aymantaybi@users.noreply.github.com>
2022-11-30 23:36:26 +0100

    Update Query.ts

--- src/schema/resolvers/Query.ts
+++ src/schema/resolvers/Query.ts
@@ -1,32 +1,41 @@
 import mongoClient from '../../mongoClient';
-
-async function logs() {
-  return [
-    {
-      address: 'String',
-      event: {
-        signature: 'String',
-        name: 'String',
-        inputs: {},
-      },
-      function: {
-        signature: 'String',
-        name: 'String',
-        inputs: {},
-      },
-      transaction: {},
-    },
-  ];
-}
+import { GraphQLYogaError } from '@graphql-yoga/node';
+import indexer from '../../indexer';
 
 async function filters(_: any, args: { tags: string[] }) {
   const { tags } = args;
-  const filters = await mongoClient
-    .db('eth-logs-indexer:parameters')
-    .collection('filters')
-    .find({ tag: { $in: tags } })
-    .toArray();
+  const query = tags.length ? { tag: { $in: tags } } : {};
+  const filters = await mongoClient.db('eth-logs-indexer:parameters').collection('filters').find(query).toArray();
   return filters;
 }
 
-export default { logs, filters };
+async function executeQuery(_: any, args: { tag: string; query: object; options: object }) {
+  const { tag, query, options } = args;
+  try {
+    const result = await mongoClient
+      .db('eth-logs-indexer:logs')
+      .collection(`tag:${tag}`)
+      .find(query || {}, options || {})
+      .toArray();
+    console.log({ result });
+    return result;
+  } catch (error: any) {
+    throw new GraphQLYogaError(error);
+  }
+}
+
+async function logsCounts(_: any, args: { tags: string[] }) {
+  const { tags } = args;
+  try {
+    const result = await Promise.all(tags.map((tag) => mongoClient.db('eth-logs-indexer:logs').collection(`tag:${tag}`).estimatedDocumentCount()));
+    return result;
+  } catch (error: any) {
+    throw new GraphQLYogaError(error);
+  }
+}
+
+function chainId() {
+  return indexer.chainId;
+}
+
+export default { filters, executeQuery, logsCounts, chainId };
