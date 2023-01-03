a8b8470abbf01f57650000af72d8bca0113a6434
aymantaybi <42820584+aymantaybi@users.noreply.github.com>
2023-01-03 21:18:16 +0100

    fix: verify if unique indexes exists before creating new ones

--- src/index.ts
+++ src/index.ts
@@ -67,9 +67,13 @@ main()
   .then(async () => {
     await mongoClient.connect();
     await logsCollection.createIndex({ filterId: 1 });
-    await logsCollection.createIndex({ 'transaction.blockNumber': 1, 'transaction.transactionIndex': 1, logIndex: 1 }, { unique: true });
-    await filtersCollection.createIndex({ id: 1 }, { unique: true });
     await filtersCollection.createIndex({ chainId: 1 });
+    if ((await logsCollection.indexExists('transaction.blockNumber_1_transaction.transactionIndex_1_logIndex_1')) === false) {
+      await logsCollection.createIndex({ 'transaction.blockNumber': 1, 'transaction.transactionIndex': 1, logIndex: 1 }, { unique: true });
+    }
+    if ((await filtersCollection.indexExists('id_1')) === false) {
+      await filtersCollection.createIndex({ id: 1 }, { unique: true });
+    }
     await indexer.initialize();
   })
   .catch((e) => {
